b95dd19d0e803483baa00a97e1db6a7f21d22b45
Masarah Fallath <masarah@MacBook-Air-alkhas-b-Masarah.local>
2018-11-30 17:57:53 +0300

    rad1

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    /// player info:
+   
+    $('.instru').on('click',function(){
+        alert("Stop breast-feeding 3 weeks before receiving radioiodine dose");
+
+   
+   })
+})
